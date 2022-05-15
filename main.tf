f4d8f195cdc81b06697ccded84f0698ab27a1805
kalirajan619 <kalirajan619@gmail.com>
2022-05-15 11:40:58 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,8 +6,8 @@ resource "aws_db_instance" "myrdss" {
   instance_class       = "db.t2.micro"
   name                 = "mydb"
   username             = "foo"
-  password             = file("../password.txt")
-  #password = "faugfufagu"
+  #password             = file("../password.txt")
+  password = "faugfufagu"
   parameter_group_name = "default.mysql5.7"
   skip_final_snapshot = "true"
 }
