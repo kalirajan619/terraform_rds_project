d6e03972326a585d2aeeac8503e1d35141af3a89
kalirajan619 <kalirajan619@gmail.com>
2022-05-15 11:58:53 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -30,6 +30,11 @@ variable "password" {
 #   skip_final_snapshot = "true"
 # }
 
-resource "aws_eip" "myeip" {
-  vpc = "true"
+# resource "aws_eip" "myeip" {
+#   vpc = "true"
+# }
+resource "aws_instance" "instance1" {
+  ami           = "ami-0f1fb91a596abf28d"
+  instance_type = "t2.micro"
+ 
 }
