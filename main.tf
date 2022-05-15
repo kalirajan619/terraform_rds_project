87a4acfbaacfbefd604ab109acefd52f68718530
kalirajan619 <kalirajan619@gmail.com>
2022-05-15 11:50:43 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,16 +16,20 @@ variable "password" {
 
 }
 
-resource "aws_db_instance" "myrdss" {
-  allocated_storage    = 5
-  storage_type         = "gp2"
-  engine               = "mysql"
-  engine_version       = "5.7"
-  instance_class       = "db.t2.micro"
-  db_name                 = "mydb"
-  username             = "foo"
-  #password             = file("../password.txt")
-  password = var.password
-  parameter_group_name = "default.mysql5.7"
-  skip_final_snapshot = "true"
+# resource "aws_db_instance" "myrdss" {
+#   allocated_storage    = 5
+#   storage_type         = "gp2"
+#   engine               = "mysql"
+#   engine_version       = "5.7"
+#   instance_class       = "db.t2.micro"
+#   db_name                 = "mydb"
+#   username             = "foo"
+#   #password             = file("../password.txt")
+#   password = var.password
+#   parameter_group_name = "default.mysql5.7"
+#   skip_final_snapshot = "true"
+# }
+
+resource "aws_eip" "myeip" {
+  vpc = "true"
 }
