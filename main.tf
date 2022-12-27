b6b9619d41536d2214e53e15b435e46feef9a4ed
kalirajan619 <kalirajan619@gmail.com>
2022-12-27 20:31:32 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,40 +1,13 @@
-provider aws {
-region     = "ap-south-1"
-access_key = var.access_key
-secret_key = var.secret_key
-}
-
-variable "access_key" {
-
-}
-
-variable "secret_key" {
-
-}
-
-variable "password" {
-
-}
-
-# resource "aws_db_instance" "myrdss" {
-#   allocated_storage    = 5
-#   storage_type         = "gp2"
-#   engine               = "mysql"
-#   engine_version       = "5.7"
-#   instance_class       = "db.t2.micro"
-#   db_name                 = "mydb"
-#   username             = "foo"
-#   #password             = file("../password.txt")
-#   password = var.password
-#   parameter_group_name = "default.mysql5.7"
-#   skip_final_snapshot = "true"
-# }
-
-# resource "aws_eip" "myeip" {
-#   vpc = "true"
-# }
-resource "aws_instance" "instance1" {
-  ami           = "ami-0f1fb91a596abf28d"
-  instance_type = "t2.micro"
- 
+resource "aws_db_instance" "myrdss" {
+  allocated_storage    = 20
+  storage_type         = "gp2"
+  engine               = "mysql"
+  engine_version       = "5.7"
+  instance_class       = "db.t2.micro"
+  db_name                 = "mydb"
+  username             = "foo"
+  #password             = file("../password.txt")
+  password = "faugfufagu"
+  parameter_group_name = "default.mysql5.7"
+  skip_final_snapshot = "true"
 }
