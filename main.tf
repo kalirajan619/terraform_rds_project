aa6f936f9522049c4ca98a510540a1087b8a004e
kalirajan619 <kalirajan619@gmail.com>
2023-01-16 20:56:30 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,9 @@
+provider "aws" {
+  region     = var.region
+  access_key = var.access_key
+  secret_key = var.secret_key
+}
+
 resource "aws_instance" "instance1" {
   ami           = "ami-0f1fb91a596abf28d"
   instance_type = "t2.micro"
